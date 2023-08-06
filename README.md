11576c7ea06f988f180fab1b5a2b33b551d7f4c8
Ward Vanwalleghem <wardvanwalleghem@live.be>
2023-08-06 16:22:56 +0200

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Automatic_Testing
-Automatic testing basics using Cypress
+# Cypress Test Suite for Basic Auth and Horizontal Slider Functionality
+
+This test suite verifies the functionality of 'Basic Authentication', 'Horizontal Slider' and 'Key presses' feature on the-internet website using Cypress.
+Link: https://the-internet.herokuapp.com/
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Make sure you have Node.js and npm installed.
+3. Navigate to the project directory in your terminal.
+4. Run `npm install cypress` to install cypress.
+5. If that doesn't work, navigate to node_modules/.bin and `npm install cypress` there
+
+## Running Tests
+
+1. After installing dependencies, run `npx cypress open` to open the Cypress Test Runner.
+2. Choose 'E2E Testing'
+3. Select the desired browser to run the tests.
+4. The Cypress Test Runner will open, showing the list of available test files.
+5. Choose the-Internet.spec.cy.js
+6. Observe the test execution in the browser window.
+
+## Test Structure
+
+The test suite is organized as follows:
+
+- `basic_auth_spec.js`: Contains tests for Basic Authentication functionality.
+- `horizontal_slider_spec.js`: Contains tests for Horizontal Slider functionality.
+- `key_presses_spec.js`: Contains tests for Key Presses functionality.
+
+## Gitignore
+
+This project's `.gitignore` file has been configured to exclude certain files and directories from version control. The following files and directories are intentionally ignored:
+
+- `node_modules/`: Ignoring the `node_modules` directory where external dependencies are installed.
+
+##FIXME:
+
+- Popup prompt for `Basic Auth` doesn't get automatically declined. A manual click is obligatory.
--- cypress/e2e/the-Internet.spec.cy.js
+++ cypress/e2e/the-Internet.spec.cy.js
@@ -1,38 +1,41 @@
-//TODO: Logout basic auth: Auto-decline popup prompt
-//TODO: Horizontal Slider: verify slider.value==#range.value on screen
-
 describe("Basic Auth functionality", () => {
-    it('Login basic auth', () => {
-        cy.log('Handle basic authentication with the credentials in the URL like this: https://username:password@www.example.com/'); //clarification
-        cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth'); // Visiting the page with basic authentication
-        cy.contains('Congratulations! You must have the proper credentials.').should('be.visible'); // Verifying the successful authentication message
+    it("Should log in using basic auth", () => {
+        cy.log("Handle basic authentication with the credentials in the URL like this: https://username:password@www.example.com/");
+        cy.visit("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        cy.contains("Congratulations! You must have the proper credentials.").should("be.visible"); // Verifying the successful authentication message
     });
-    it('Logout basic auth', () => {
-        cy.log('Simulate logout by clearing cookies and re-authenticating'); //clarification
+
+    it("Should log out of basic auth", () => {
+        cy.log("Simulate logout by clearing cookies and re-authenticating");
         cy.clearCookies(); // Clearing cookies
         cy.clearLocalStorage(); // Clearing local storage
-        cy.visit('https://the-internet.herokuapp.com');
+        cy.visit("https://the-internet.herokuapp.com");
         cy.contains("Basic Auth").click(); // Clicking the "Basic Auth" link to trigger re-authentication
     });
 });
 
 describe("Horizontal Slider functionality", () => {
-    it('Verify all possible slider states', () => {
-        cy.visit('https://the-internet.herokuapp.com/horizontal_slider');
-        cy.get('input[type="range"]').as('slider'); // Getting the slider input element and giving it an alias
+    it("Should verify all possible slider states", () => {
+        cy.visit("https://the-internet.herokuapp.com/horizontal_slider");
+        cy.get("input[type='range']").as("slider"); // Getting the slider input element and giving it an alias
 
-        for (let value = 0; value <= 5; value++) {
-            cy.get('@slider').invoke('val', value).trigger('input'); // Changing slider value //* Instead of sliding the slider, which changes the sliders value, we manipulate the value, causing the slider to slide
-            cy.get('@slider').should('have.value', value.toString()); // Verifying slider value
+        for (let value = 0; value <= 5; value += 0.5) { // Loop through slider values from 0 to 5 with a step of 0.5
+            cy.get("@slider").invoke("val", value).trigger("input"); // Changing slider value
+            cy.get("@slider").should("have.value", value.toString()); // Verifying slider value
         }
     });
 });
 
 describe("Key Presses functionality", () => {
-    it('Verify displayed key on key press', () => {
-        cy.visit('https://the-internet.herokuapp.com/key_presses');
-        cy.get('#target').as('inputField'); // Getting the input element for typing and giving it an alias
-        cy.get('@inputField').type('a'); // Typing the key 'a' into the input field
-        cy.get('#result').should('have.text', `You entered: A`); // Verifying that the displayed key matches the pressed key
+    it("Should verify displayed key on key press", () => {
+        cy.visit("https://the-internet.herokuapp.com/key_presses");
+        cy.get("#target").as("inputField"); // Getting the input element for typing and giving it an alias
+
+        const keysToTest = [..."abcdefghijklmnopqrstuvwxyz0123456789"]; // List of keys to test
+        keysToTest.forEach((key) => { // Loop through each key and type it, then verify the displayed result
+            cy.get("@inputField").type(key); // Type the current key
+            const capitalizedKey = key.toUpperCase();
+            cy.get("#result").should("have.text", `You entered: ${capitalizedKey}`); // Verify the displayed key matches the pressed key
+        });
     });
 });
